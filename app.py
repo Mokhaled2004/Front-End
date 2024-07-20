bb7b7e0c046457d409141699aa15e0d44ad67128
Mokhaled2004 <mokhaled2004@gmail.com>
2024-07-20 22:42:48 +0300

    header and homepage done

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py (Flask Example)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/header')
+def header():
+    return render_template('header.html')
+if __name__ == '__main__':
+    app.run(debug=True)
